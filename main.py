448cb23baeb90cc7e50e29f09a52cef8cf230732
nipshot <40479851+nipshot@users.noreply.github.com>
2018-07-05 13:58:21 -0500

    hiworld project

---
+++ main.py
@@ -0,0 +1,70 @@
+print("hello world!!!")
+python = "newtoprogramming"
+def main():
+    print("Hi World!")
+
+main()
+def main2():
+    print(python)
+
+main2()
+main2()
+
+def main3():
+    print("epicmamay")
+
+main3()
+
+def main4(main4):
+    print(oof)
+oof= "main4"
+main4(oof)
+
+def add(num1, num2):
+    """returns num1 plus num2"""
+    return num1 + num2
+
+def mane():
+    print(add(2, 3))
+
+mane()
+
+def multiply(num1, num2):
+    """returns numX times numY"""
+    return num1 * num2
+
+def subtract(num1, num2):
+    """returns numX minus numY"""
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def man():
+    print(multiply(2, 5)) #10
+    print(subtract(10, 7)) #3
+
+man()
+
+def calc():
+    num1= int(input("What is num1?"))
+    num2 = int(input("What is num2?"))
+    MyResult = add(num1, num2)
+    print(MyResult)
+
+calc()
+
+def calc2():
+    num1 = int(input("what is num1?"))
+    num2 = int(input("what is num2?"))
+    print(add(num1, num2))
+    print(subtract(num1, num2))
+    print(multiply(num1, num2))
+    print(divide(num1, num2))
+calc2()
+
+def bottomtext():
+    print('Hello' + 'World' + '!')
+    print('Alex' * 6)
+    print('1' + '2')
+bottomtext()
